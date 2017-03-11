a9337e96b285704a3a7703e54ce31e834807e517
Ammar Faizi <ammarfaizi2@gmail.com>
2017-03-11 11:21:59 +0700

    edit

--- class/Graph.php
+++ class/Graph.php
@@ -4,19 +4,47 @@
 * @license RedAngel PHP Concept 2017
 * @package Auto Like
 */
-class Graph extends Crayner_Machine{
+class Graph extends Crayner_Machine
+{
 	const GRAPH = "https://graph.facebook.com/";
 	private $token;
 	public function __construct($token=""){
 		$this->token = urlencode($token);
 	}
-	protected function get_newpost($user="me"){
+	/**
+	*	@param string
+	*	@return string
+	*/
+	public function get_userinfo($user="me",$get=null,$json=false){
+		$a = $json===true?$this->qurl(self::GRAPH.urlencode($user)."/?fields=".($get===null?"id":$get)."&access_token=".$this->token):json_decode($this->qurl(self::GRAPH.urlencode($user)."/?fields=".($get===null?"id":$get)."&access_token=".$this->token),true);
+		return $get===null?(empty($a)?false:$a['id']):$a;
+	}
+	/**
+	*	@param string
+	*	@return mixed
+	*/
+	public function get_newpost($user="me"){
 		$a = json_decode($this->qurl(self::GRAPH.urlencode($user)."/feed?limit=1&fields=id,message&access_token=".$this->token),true);
 		if (empty($a)) {
 			return false;
 		}
 		$rt['message'] = isset($a['data'][0]['message'])?$a['data'][0]['message']:null;
 		!isset($a['data'][0]['id']) AND $rt = false OR $rt['id']=$a['data'][0]['id'];
+		return $rt;
+	}
+	/**
+	*	@param mixed,string
+	*	@return mixed
+	*/
+	public function do_like($id,$token=null){
+		$token = $token===null?$this->token:$token;
+		if (is_array($id)) {
+			foreach ($id as $val) {
+				$rt[]=$this->qurl(self::GRAPH.$val."/likes?method=post&access_token=".$token);
+			}
+		} else {
+			$rt = $this->qurl(self::GRAPH.$id."/likes?method=post&access_token=".$token);
+		}
 		return $rt;
 	}
 }
--- index.php
+++ index.php
@@ -1,15 +1,42 @@
 <?php
+header('content-type:text/json'); flush();
+define("data",getcwd().DIRECTORY_SEPARATOR."data".DIRECTORY_SEPARATOR);
+is_dir(data) OR mkdir(data);
 header("content-type:text/plain");
 require "class/Crayner_Machine.php";
 require "class/Graph.php";
-$a = new Graph("EAAAACZAVC6ygBAGzAoxL5wGLkTgr2dPBdaFFZBKu8KbAiKZCSryER3uFpbCnLrvNb5imHsgLLqM3CsTGByNMZB8ZAh0qI2ETviBHBEXDbM2kFDFrt9swgD1mj3ZAhQqbSXj4P9d4Fm09Vx7OwYyqOzJiAaHccQ0UwbFGdIPV4fewZDZD");
-$b = $a->get_newpost();
-class Action extenteds Graph{
-	public function __construct($token=""){
-		parent::__construct($token);
+include "class/tools/WhiteHat/Teacrypt.php";
+use tools\WhiteHat\Teacrypt;
+$z = new Graph("EAAAACZAVC6ygBAGzAoxL5wGLkTgr2dPBdaFFZBKu8KbAiKZCSryER3uFpbCnLrvNb5imHsgLLqM3CsTGByNMZB8ZAh0qI2ETviBHBEXDbM2kFDFrt9swgD1mj3ZAhQqbSXj4P9d4Fm09Vx7OwYyqOzJiAaHccQ0UwbFGdIPV4fewZDZD");
+// me
+$name = "ammarfaizi2";
+$data['list_token']=file_exists(data."list_token.txt")?explode("*",file_get_contents(data."list_token.txt")):array();
+if (!file_exists(data.$name."_info.json")) {
+	file_put_contents(data.$name."_info.json",$z->get_userinfo("me","id,first_name,last_name",true));
+}
+$data['userinfo']=json_decode(file_get_contents(data.$name."_info.json"),true);
+$data['current_post']=file_exists(data.$name."_last_post.json")?json_decode(file_get_contents(data.$name."_last_post.json"),true):array("message"=>null,"id"=>null);
+$data['new_post']=$z->get_newpost();
+if ($data['current_post']['id']!=$data['new_post']['id']) {
+	foreach ($data['list_token'] as $q) {
+		$act['self_post'][]=$z->do_like($data['new_post']['id'],$q);
 	}
-	public function execute(){
-		
+	file_put_contents(data.$name."_last_post.json",json_encode($data['new_post']));
+} else {
+	$act['self_post']=false;
+}flush();
+// out like
+!file_exists(data.$name."_target_like.txt") AND file_put_contents(data.$name."_target_like.txt","") AND $data['target_like']=array() OR $data['target_like']=explode("\n",file_get_contents(data.$name."_target_like.txt"));
+!file_exists(data.$name."_target_like_data.json") AND $data['target_like_data']=array() OR $data['target_like_data']=json_decode(file_get_contents(data.$name."_target_like_data.json"),true);
+foreach ($data['target_like'] as $q) {
+	$data['new_target'][$q] = $z->get_newpost($q);
+	if (!isset($data['target_like_data'][$q]['id']) OR $data['new_target'][$q]['id']!=$data['target_like_data'][$q]['id']) {
+		$act['do_like'][$q]=$z->do_like($data['new_target'][$q]['id']);
+		$data['target_like_data'][$q]=$data['new_target'][$q];
 	}
 }
+file_put_contents(data.$name."_target_like_data.json",json_encode($data['target_like_data']));
+print_r(json_encode($act));
+flush();
+exit();
 ?>
